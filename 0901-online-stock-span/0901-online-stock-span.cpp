dc33b73fd343c77603d58485b35cfe2382cd7bc6
Prakhar Singh <92270107+prolion1@users.noreply.github.com>
2022-11-10 00:36:15 +0530

    Time: 659 ms (5.06%), Space: 84.4 MB (22.76%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -0,0 +1,24 @@
+class StockSpanner {
+public:
+    stack<pair<int, int>> st;
+
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        
+        int count = 0;
+        while(st.empty() == false && st.top().first <= price)
+        {
+            count += st.top().second;
+            st.pop();
+        }
+
+        count++;
+
+        st.push({price, count});
+
+        return count;
+    }
+};
